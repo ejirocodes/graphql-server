f8a16abbbaacc05747cb5903b32034b988b096d3
Ejiro Asiuwhu <ejiroasiuwhu10@gmail.com>
2021-08-06 19:13:33 +0100

    define schema type

--- src/schema/schema.js
+++ src/schema/schema.js
@@ -1,3 +1,12 @@
 const graphql = require('graphql');
 
-const { GraphQLObjectType } = graphql;
+const { GraphQLObjectType, GraphQLString } = graphql;
+
+const JobType = new GraphQLObjectType({
+  name: 'Job',
+  fields: () => ({
+    id: { type: GraphQLString },
+    title: { type: GraphQLString },
+    description: { type: GraphQLString },
+  }),
+});
