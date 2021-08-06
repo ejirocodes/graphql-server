440602d7471ebf760a52243c64933454225f6469
Ejiro Asiuwhu <ejiroasiuwhu10@gmail.com>
2021-08-06 19:17:49 +0100

    define graphql root query

--- src/schema/schema.js
+++ src/schema/schema.js
@@ -1,6 +1,6 @@
 const graphql = require('graphql');
 
-const { GraphQLObjectType, GraphQLString } = graphql;
+const { GraphQLObjectType, GraphQLString, GraphQLSchema } = graphql;
 
 const JobType = new GraphQLObjectType({
   name: 'Job',
@@ -9,4 +9,19 @@ const JobType = new GraphQLObjectType({
     title: { type: GraphQLString },
     description: { type: GraphQLString },
   }),
+});
+
+const RootQuery = new GraphQLObjectType({
+  name: 'RootQueryType',
+  fields: {
+    jobs: {
+      type: JobType,
+      args: { id: { type: GraphQLString } },
+      resolve(root, args) {},
+    },
+  },
+});
+
+module.exports = new graphql.GraphQLSchema({
+  query: RootQuery,
 });
